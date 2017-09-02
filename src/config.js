16d994e4b00dbbfed1fc95e5069d68dde04be45c
nya1 <nya1@protonmail.com>
2017-09-02 16:34:26 +0200

    hardcode api details

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 var config = {
-  BASE_API: process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : '/api',
+  BASE_API: process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'http://htc.washmypants.com/api/',
   BASE_AUTH: null
 }
 
